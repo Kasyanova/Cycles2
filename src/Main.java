b8afc69a076656659191f3ade9445824586abe52
KKasyanova <eliseeva.kseniya@gmail.com>
2022-10-09 15:18:31 +0500

    Задача №8_Циклы

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,24 @@
+/*Напишите программу, где пользователь вводит любое целое положительное число n. А программа суммирует все нечетные числа от 1 до введенного пользователем числа n.
+ */
+
+package com.max.idea;
+
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner in = new Scanner(System.in);
+        System.out.println("Введите любое целое положительное число n");
+        int num = in.nextInt();
+        int sum = 0; //вспомогательная переменная для результата
+
+        //прибавить к sum все значения, где остаток от деления на 2 отсутствует, т.е нечетные
+        for (int i=1;i<num;i++){
+            if (i%2!=0) {
+                sum = sum+i;
+            }
+        }
+
+        System.out.println(sum);
     }
 }
